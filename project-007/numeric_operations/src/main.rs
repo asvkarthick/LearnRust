d36eb6c1b023ccd340d8659cdc29554d1a68f9b1
Karthick Kumaran <asvkarthick@gmail.com>
2026-03-30 11:51:47 -0700

    Add Rust program to demonstrate numeric operations

---
+++ project-007/numeric_operations/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let a = 5;
+    let b = 6;
+    let c = a + b;
+    let d = b - a;
+    let e = a * b;
+    let f = a % b;
+    let g = b / a;
+    println!("a = {}", a);
+    println!("b = {}", b);
+    println!("c = {}", c);
+    println!("d = {}", d);
+    println!("e = {}", e);
+    println!("f = {}", f);
+    println!("g = {}", g);
+}
