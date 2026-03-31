e317a855d4c0da2256cdaa56847b715fb739b69f
Karthick Kumaran <asvkarthick@gmail.com>
2026-03-30 21:22:13 -0700

    Deep copy a String

--- project-011/ownership/src/main.rs
+++ project-011/ownership/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let s2 = s1; // s2 owns "Hello"
     println!("s2 = {}", s2);
     // println!("s1 = {}", s1); // Uncommenting this will result in compilation error
+    
+    let s3 = String::from("World");
+    let s4 = s3.clone(); // s4 now owns a copy of s3
+    println!("s3 = {}", s3);
+    println!("s4 = {}", s4);
 }
