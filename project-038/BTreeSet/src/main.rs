e2eef38402ab84268573a5ce1b54cf16b6979d9e
Karthick Kumaran <asvkarthick@gmail.com>
2026-04-02 00:43:02 -0700

    BTreeSet example

---
+++ project-038/BTreeSet/src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::BTreeSet;
+
+fn main() {
+    println!("BTreeSet example!");
+    let mut set = BTreeSet::new();
+
+    // Adding elements
+    set.insert(30);
+    set.insert(20);
+    set.insert(10);
+    set.insert(50);
+    set.insert(40);
+
+    // Iterating elements
+    for val in &set {
+        print!("{val} ");
+    }
+    println!();
+
+    // Removing elements
+    set.remove(&30);
+
+    // Range queries
+    for val in set.range(15..=40) {
+        print!("{val} ");
+    }
+    println!();
+
+    println!("First: {:?}", set.first());
+    println!("last:  {:?}", set.last());
+
+    let set2: BTreeSet<i32> = [20, 60, 80].into();
+    let union: BTreeSet<_> = set.union(&set2).collect();
+    println!("union: {union:?}");
+
+    set.pop_first();
+    set.pop_last();
+}
